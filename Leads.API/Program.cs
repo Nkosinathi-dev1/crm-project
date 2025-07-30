dcab11e538a3ca319d8ef839ff1afd1d4954ca64
Nkosinathi-dev1 <nkosinathi.ngele.dev@gmail.com>
2025-07-30 22:19:30 +0200

    added migrations

--- Leads.API/Program.cs
+++ Leads.API/Program.cs
@@ -1,10 +1,28 @@
+using Leads.Core.Interfaces;
+using Leads.DataAccess.Data;
+using Leads.DataAccess.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Getting the connection string from appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//Register ofr DataContext with SQL Server provider 
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(connectionString));  // or UseNpgsql() if using PostgreSQL in future
+
+//Register of generic repository for IRepository<T>
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+
 
 var app = builder.Build();
 
